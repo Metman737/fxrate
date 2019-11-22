e8d32a079201db0148d562240fab9973e1fa1350
Leon Burmeister <leonburmeister737@icloud.com>
2019-11-22 17:57:57 +0100

    display model in home view

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from home.models import Post
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    posts = Post.objects.filter(status=1).order_by('-creation_date')
+    return render(request, 'home/index.html', {'posts': posts})
