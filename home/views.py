c4fa105673228e5105bf06833d11fb8ae6a2fdc4
amiri <yusef.amiri@tuhh.de>
2019-11-29 15:20:08 +0100

    add exchange detaile page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from home.models import Post
+from home.models import Post, ExchangeRate
 
 
 # Create your views here.
 def index(request):
     posts = Post.objects.filter(status=1).order_by('-creation_date')
     return render(request, 'home/index.html', {'posts': posts})
+
+
+def table_detail(request):
+    exchengerates = ExchangeRate
+    return render(request, 'home/exchangerate_detail.html', {'exchangerates': exchengerates})
