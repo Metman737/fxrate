ccf494692265db348e1dbc2749b69a6bf0cfbfeb
Leon Burmeister <leonburmeister737@icloud.com>
2019-12-17 19:15:24 +0100

    remove spaces

--- home/views.py
+++ home/views.py
@@ -1,7 +1,5 @@
-from django.db.models import Max, Subquery, OuterRef, Prefetch
 from django.views.generic import DetailView, ListView
-
-from home.models import Post, ExchangeRate, Currency
+from home.models import Post, ExchangeRate
 
 
 class PostList(ListView):
@@ -18,11 +16,11 @@ class ExchangeRateTable(ListView):
     # finds the latest exchangerate row for each currency
     # TODO: Disable caching
     # TODO: Find django method instead of SQL query
-    queryset = ExchangeRate.objects.raw( '''SELECT c.*, e1.*
+    queryset = ExchangeRate.objects.raw('''SELECT c.*, e1.*
     FROM home_currency c
     JOIN home_exchangerate e1 ON (c.id = e1.currency_id)
     LEFT OUTER JOIN home_exchangerate e2 ON (c.id = e2.currency_id AND 
         (e1.change_date_time < e2.change_date_time OR (e1.change_date_time = e2.change_date_time AND e1.id < e2.id)))
-    WHERE e2.id IS NULL''' )
+    WHERE e2.id IS NULL''')
 
     template_name = 'home/exchange_rate_table.html'
