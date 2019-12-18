abe73d5997a596a8218cc373018cabc64e4f9724
Leon Burmeister <leonburmeister737@icloud.com>
2019-12-18 14:12:18 +0100

    edit homepage

--- fxrate/views.py
+++ fxrate/views.py
@@ -2,8 +2,19 @@ from django.shortcuts import redirect
 from django.views.generic import ListView
 
 from blog.models import Post
+from exchange.models import ExchangeRate
 
 
 class Homepage(ListView):
     queryset = Post.objects.filter(status=1).order_by('creation_date')[:4]
     template_name = 'homepage.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data( **kwargs )
+        context['exchangerate_list'] = ExchangeRate.objects.raw('''SELECT c.*, e1.*
+    FROM home_currency c
+    JOIN home_exchangerate e1 ON (c.id = e1.currency_id)
+    LEFT OUTER JOIN home_exchangerate e2 ON (c.id = e2.currency_id AND 
+        (e1.change_date_time < e2.change_date_time OR (e1.change_date_time = e2.change_date_time AND e1.id < e2.id)))
+    WHERE e2.id IS NULL''')
+        return context
